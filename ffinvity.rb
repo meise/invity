f2c89f61494eaa66581e3bcfd0d99559c9877d7b
Daniel Meißner <daniel@3st.mine.nu>
2011-09-07 17:17:38 +0200

    Added license information.

--- ffinvity.rb
+++ ffinvity.rb
@@ -1,7 +1,23 @@
 #!/usr/lib/env ruby
 # encoding: utf-8
+=begin
+Copyright Daniel Meißner <dm@3st.be>, 2011
 
+This file is part of Invity to send email notifications to upcoming Freifunk events.
 
+This script is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This Script is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Invity. If not, see <http://www.gnu.org/licenses/>.
+=end
 
 require 'mechanize'
 require 'active_support/core_ext'
